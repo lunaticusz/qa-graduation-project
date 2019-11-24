86d8db5cdc64f77be37465d6ed6872de53f3c242
E.Igonichkina <e.igonichkina@magdv.com>
2019-11-24 21:00:45 +0700

    переходы между страницами (section4/lesson2/step9)

--- test_main_page.py
+++ test_main_page.py
@@ -9,10 +9,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_url()
-    login_page.should_be_login_form()
-    login_page.should_be_register_form()
 
 
 def test_guest_should_see_login_link(browser):
