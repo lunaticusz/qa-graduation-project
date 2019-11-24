a2d80af034d06e9fb3212a01f85f4fd1cfe3cd07
E.Igonichkina <e.igonichkina@magdv.com>
2019-11-24 15:37:23 +0700

    добавлена обработка страницы логина (section4/lesson2/step8)

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 # Команда для запуска:
 # pytest -v --tb=line --language=en test_main_page.py
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
@@ -8,6 +9,10 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
 
 
 def test_guest_should_see_login_link(browser):
