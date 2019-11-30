a8d706922321fc058158cbe089006f5b012bee61
E.Igonichkina <e.igonichkina@magdv.com>
2019-11-30 21:14:03 +0700

    решение задачи с добавлением книги в корзину (section4/lesson3/step2)

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators():
     LOGIN_URL = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
     REGISTER_FORM = (By.ID, "register_form")
     LOGIN_FORM = (By.ID, "login_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form .btn-add-to-basket")
+    TITLE_IN_PRODUCT_INFO = (By.CSS_SELECTOR, ".product_main h1")
+    TITLE_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert-success:nth-child(1) strong")
+    PRICE_IN_PRODUCT_INFO = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRICE_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-info strong")
---
+++ pages/product_page.py
@@ -0,0 +1,30 @@
+from pages.locators import ProductPageLocators
+from .base_page import BasePage
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        self.should_be_button_add_to_cart()
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        button.click()
+        self.solve_quiz_and_get_code()
+
+    def should_be_button_add_to_cart(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Button 'Add to basket' is not presented"
+
+    def should_be_added_to_cart(self):
+        # Сообщение о том, что товар добавлен в корзину. Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
+        assert self.is_element_present(*ProductPageLocators.TITLE_IN_PRODUCT_INFO), "Product title is not presented in product info"
+        assert self.is_element_present(*ProductPageLocators.TITLE_IN_SUCCESS_MESSAGE), "Message with title is not presented"
+
+        title_in_product_info = self.get_element_text(*ProductPageLocators.TITLE_IN_PRODUCT_INFO)
+        title_in_success_message = self.get_element_text(*ProductPageLocators.TITLE_IN_SUCCESS_MESSAGE)
+        assert title_in_product_info == title_in_success_message, "Product title is not correct in success message"
+
+        # Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара.
+        assert self.is_element_present(*ProductPageLocators.PRICE_IN_PRODUCT_INFO), "Product price is not presented in product info"
+        assert self.is_element_present(*ProductPageLocators.PRICE_IN_SUCCESS_MESSAGE), "Message with price is not presented"
+
+        price_in_product_info = self.get_element_text(*ProductPageLocators.PRICE_IN_PRODUCT_INFO)
+        price_in_success_message = self.get_element_text(*ProductPageLocators.PRICE_IN_SUCCESS_MESSAGE)
+        assert price_in_product_info == price_in_success_message, "Product price is not correct in success message"
