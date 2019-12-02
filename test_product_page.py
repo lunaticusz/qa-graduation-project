954ab5ef5d528d624d996fb0da5288f60b639440
E.Igonichkina <e.igonichkina@magdv.com>
2019-12-02 20:24:19 +0700

    наследование, действия с логином вынесены в класс с базовой страницей (section4/lesson3/step8)

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,6 @@
 import pytest
+from pages.locators import BasePageLocators
+from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
 PRODUCT_LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -46,3 +48,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
     # Проверяем, что нет сообщения об успехе с помощью is_disappeared
     page.should_success_message_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, BasePageLocators.LOGIN_LINK)
+    login_page.should_be_login_page()
