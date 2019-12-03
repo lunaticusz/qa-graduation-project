38c27a9db45cfc74614e7d0601b55cf63b94e833
E.Igonichkina <e.igonichkina@magdv.com>
2019-12-03 23:08:50 +0700

    в файле с тестами на главную страницу тесты на логин сгруппированы в классе с меткой (section4/lesson3/step11)

--- test_main_page.py
+++ test_main_page.py
@@ -1,25 +1,28 @@
 # Команда для запуска:
 # pytest -v --tb=line --language=en test_main_page.py
+# pytest -v --tb=line --language=en -m login_guest test_main_page.py
+import pytest
 from pages.basket_page import BasketPage
 from pages.locators import MainPageLocators
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = MainPageLocators.MAIN_PAGE_URL
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_url()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = MainPageLocators.MAIN_PAGE_URL
+        page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_url()
 
-
-def test_guest_should_see_login_link(browser):
-    link = MainPageLocators.MAIN_PAGE_URL
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = MainPageLocators.MAIN_PAGE_URL
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
